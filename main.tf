1cdd9fc51070c5cb7b9f3ec7c89b0cc64fc77f35
Daksh Devrani <dakshdevrani2789@gmail.com>
2026-08-26 22:22:52 +0530

    invalid security

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ resource "aws_security_group" "web_sg" {
   ingress {
     description = "SSH from anywhere"
 
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
   }
