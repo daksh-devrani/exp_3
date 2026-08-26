de1abd13dfa5ae383f11e68544057b9f1612382d
Daksh Devrani <dakshdevrani2789@gmail.com>
2026-08-26 22:14:49 +0530

    invalid security

--- main.tf
+++ main.tf
@@ -1,35 +1,29 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.13.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-}
-}
+    }
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
-  description = "Security group for the web server"
+  description = "Security group for web server"
 
   ingress {
     description = "SSH from anywhere"
+
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  egress {
-    description = "Allow outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
